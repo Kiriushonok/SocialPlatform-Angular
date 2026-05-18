2015cffd594a3efc175e137e37af1ecb919587c1
Kiriushonok <89874515280@yandex.ru>
2026-05-18 15:04:08 +0400

    Added button component and finished profile card

--- tik-talk/src/app/shared/components/profile-card/profile-card.ts
+++ tik-talk/src/app/shared/components/profile-card/profile-card.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SkillTag } from '../skill-tag/skill-tag';
+import { Button } from "../button/button";
 
 @Component({
   selector: 'app-profile-card',
-  imports: [SkillTag],
+  imports: [SkillTag, Button],
   templateUrl: './profile-card.html',
   styleUrl: './profile-card.scss',
 })
